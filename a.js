701abc6d1b99792910d598b05fe0ba601e3b4d9e
Nandha <mail.nandha30@gmail.com>
2017-12-11 11:09:18 +0530

    changes

--- a.js
+++ a.js
@@ -3,5 +3,5 @@ var zgidval = SupportUI.CurrentPortal.zgid;
 var depval = SupportUI.Departments.getActiveDepartmentObj()[0].depid;
 var dataval = '{"firstName":"","lastName":"Anonymous","associatedDepartmentIds":["'+depval+'"],"associatedChatDepartmentIds":[""],"perType":"Admin","aboutInfo":"","mobile":"","phone":"","extn":"","langCode":"en_US","timeZone":"Asia/Kolkata","countryCode":"en_US","status":"ACTIVE","emailId":"nandhakumar.s+hack@zohocorp.com"}'
 dataval = JSON.parse(dataval);
-jQuery.ajax({url:'http://nandha-zt82.tsi.zohocorpin.com:8708/api/v1/agents?orgId='+zgidval,type:'post',data: JSON.stringify(dataval),
+jQuery.ajax({url:'https://supportsecurity.localzoho.com/api/v1/agents?orgId='+zgidval,type:'post',data: JSON.stringify(dataval),
             contentType: "application/json; charset=utf-8",headers:{"X-ZCSRF-TOKEN":csrfcookieval}});
